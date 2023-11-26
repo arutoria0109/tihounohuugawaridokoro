facc91f6eb383874fe2d8d0ecb4c4625b6971985
EC2 Default User <samanimaya017@gmail.com>
2023-11-26 07:57:35 +0000

    タイムゾーンを東京に変更

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :member
   belongs_to :store
 
+  validates :comment, presence: true
+
 end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,13 @@ module Tihounohuugawaridokoro
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    # タイムゾーンを日本時間に設定
+    config.time_zone = 'Tokyo'
+
+    # DBに保存する時刻も日本時間に設定
+    config.active_record.default_timezone = :local
+
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
