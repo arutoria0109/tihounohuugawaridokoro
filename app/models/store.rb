bacdcdd2b5477791f1fe7b8043937f3d738fa5fe
EC2 Default User <samanimaya017@gmail.com>
2023-12-01 13:32:19 +0000

    写真投稿の修正

--- app/models/store.rb
+++ app/models/store.rb
@@ -59,13 +59,4 @@ class Store < ApplicationRecord
     like_lists.exists?(member_id: member.id)
   end
 
-  def get_image(x,y)
-    unless image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
-    end
-    image.variant(resize_to_limit: [x, y]).processed
-
-  end
-
 end
